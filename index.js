df9aaba9f8831cb45b08c767da8b76c255a5ea97
Michaela Jakubikova <jakubi.mich@gmail.com>
2024-03-08 18:50:07 +0100

    prompt na dovednosti

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('funguju!');
+const html = Number(prompt("Na škále od 0-100, jak zvládáš HTML?"))
+const css = Number(prompt("Na škále od 0-100, jak zvládáš CSS?"))
+const js = Number(prompt("Na škále od 0-100, jak zvládáš JavaScript?"))
+
+
