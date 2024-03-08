f3b3b3cb5fa612b9c4349521da467d0ec09bd59d
Michaela Jakubikova <jakubi.mich@gmail.com>
2024-03-08 19:22:33 +0100

    funkce na prepsani dovednosti na skale na zaklade promptu od uzivatele

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ let updateSkill = (html, css, js) => {
     const cssZapis = document.querySelector("#skill2 .skill__value")
     const jsZapis = document.querySelector("#skill3 .skill__value")
 
+    const htmlBar = document.querySelector("#skill1 .skill__progress")
+    const cssBar = document.querySelector("#skill2 .skill__progress")
+    const jsBar = document.querySelector("#skill3 .skill__progress")
+
     htmlZapis.textContent = html + " / 100"
     cssZapis.textContent = css + " / 100"
     jsZapis.textContent = js + " / 100"
+
+    htmlBar.style.width = `${html}%`
+    cssBar.style.width = `${css}%`
+    jsBar.style.width = `${js}%`    
 }
     
 updateSkill(html, css, js)
